14ad51ecc0e1a9e1d81ad7500fde8f3112cae9c9
Ibragimov <ib_maga_06@mail.ru>
2025-02-19 09:49:48 +0300

    added file 'buttons'

---
+++ lexicon/buttons.py
@@ -0,0 +1,38 @@
+"""название и значение кнопок"""
+
+BUTTONS: dict[str, dict] = {
+    "menu": {
+        # 'products': 'Продукты',
+        "rates": "Ставки",
+        "warehouses": "Склады",
+        "settings": "Настройки",
+        "help": "Помощь",
+    },
+    # 'products_kbd': {
+    #     'send_index': 'Отправить артикул',
+    #     'back': 'Назад',
+    # },
+    # 'send_index_kbd': {
+    #     'back': 'Назад'
+    # },
+    "rates_kbd": {
+        "view_rates": "Просмотр",
+        "charge_rates": "Изменить",
+        "automation_rates": "Автоматизация",
+        "back": "Назад",
+    },
+    "warehouses_kbd": {
+        "view_warehouses": "Просмотр",
+        "notice_warehouses": "Уведомление",
+        "automation_warehouses": "Автоматизация",
+        "back": "Назад",
+    },
+    "view_warehouses_kbd": {"back": "Назад"},
+    "settings_kbd": {
+        "api_keys": "API ключи",
+        "frequency": "Частота обновления",
+        "back": "Назад",
+    },
+    "api_keys_kbd": {"back": "Назад"},
+    "help_kbd": {"back": "Назад"},
+}
