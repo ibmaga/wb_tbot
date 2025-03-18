0db4ca812aea621d6614bd2e7ba05b52e2ab07a7
Ibragimov <ib_maga_06@mail.ru>
2025-03-18 17:17:32 +0300

    added new CRUD operations

--- db/dao/base.py
+++ db/dao/base.py
@@ -1,8 +1,39 @@
+from typing import TypeVar, Generic, Type, Optional, Any
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.expression import Select, Update
+
+from db.models.base import Base
+
+ModelType = TypeVar("ModelType", bound=Base)
 
 
-class BaseDAO:
-    model = None
+class BaseDAO(Generic[ModelType]):
+    model: Type[ModelType] = None
 
     def __init__(self, session: AsyncSession):
         self.session = session
+
+    async def create(self, data: dict[str, Any]) -> ModelType:
+        instance = self.model(**data)
+        self.session.add(instance)
+        await self.session.commit()
+        return instance
+
+    async def get(self, id: int) -> Optional[ModelType]:
+        stmt: Select = select(self.model).where(self.model.id == id)
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def update(self, id: int, data: dict[str, Any]) -> Optional[ModelType]:
+        stmt: Update = update(self.model).where(self.model.id == id).values(**data)
+        result = await self.session.execute(stmt)
+        await self.session.commit()
+        return result.scalar_one_or_none()
+
+    async def delete(self, id: int) -> Optional[ModelType]:
+        instance = await self.get(id)
+        if instance:
+            await self.session.delete(instance)
+            await self.session.commit()
+        return instance
--- db/dao/users.py
+++ db/dao/users.py
@@ -1,20 +1,20 @@
-from sqlalchemy import select
+from typing import Any
+from sqlalchemy import select, update
 
 from db.dao.base import BaseDAO
 from db.models.users import User
 
 
-class UserDAO(BaseDAO):
+class UserDAO(BaseDAO[User]):
     model = User
 
-    async def create_user(self, user):
-        user = User(**user)
-        self.session.add(user)
-
-        await self.session.commit()
-        return user
-
-    async def get_user_by_user_id(self, user_id: int):
+    async def get_user_by_user_id(self, user_id: int) -> model:
         smt = select(self.model).where(self.model.user_id == user_id)
         result = await self.session.execute(smt)
+        return result.scalar_one_or_none()
+
+    async def update_user_by_user_id(self, user_id: int, data: dict[str, Any]) -> model:
+        stmt = update(self.model).where(self.model.user_id == user_id).values(**data)
+        result = await self.session.execute(stmt)
+        await self.session.commit()
         return result.scalar_one_or_none()
