a9a8fc1a8fbd708bf5490ef43ded5e6810e9fbbf
Ibragimov <ib_maga_06@mail.ru>
2025-03-20 10:46:59 +0300

    added new rows

--- db/models/users.py
+++ db/models/users.py
@@ -11,7 +11,8 @@ class User(Base, TableNameMixin, TimeStampMixin):
     user_id: Mapped[int] = mapped_column(BIGINT, unique=True)
     username: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
     full_name: Mapped[str] = mapped_column(nullable=True)
-    access: Mapped[bool] = mapped_column(default=True, nullable=False)
+    access: Mapped[bool] = mapped_column(default=False, nullable=False)
+    banned: Mapped[bool] = mapped_column(default=False, nullable=False)
 
     def __repr__(self):
         return f"<User {self.user_id} {self.username} {self.access}>"
