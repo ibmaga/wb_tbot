924e9e4235bf9ecd316a20481cd82b2f83281ec3
Ibragimov <ib_maga_06@mail.ru>
2025-03-21 19:30:55 +0300

    refactor: fixed error with 'create' and other

--- db/dao/base.py
+++ db/dao/base.py
@@ -1,7 +1,8 @@
-from typing import TypeVar, Generic, Type, Optional, Any
+from typing import TypeVar, Generic, Type, Optional
+
 from sqlalchemy import select, update
+from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.sql.expression import Select, Update
 
 from db.models.base import Base
 
@@ -15,18 +16,19 @@ class BaseDAO(Generic[ModelType]):
         self.session = session
 
     async def create(self, **data) -> ModelType:
-        instance = self.model(**data)
-        await self.session.merge(instance)
+        stmt = insert(self.model).values(**data).on_conflict_do_nothing()
+        await self.session.execute(stmt)
+        result = self.model(**data)
         await self.session.commit()
-        return instance
+        return result
 
     async def get(self, id: int) -> Optional[ModelType]:
-        stmt: Select = select(self.model).where(self.model.id == id)
+        stmt = select(self.model).where(self.model.id == id)
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
 
     async def update(self, id: int, **data) -> Optional[ModelType]:
-        stmt: Update = update(self.model).where(self.model.id == id).values(**data)
+        stmt = update(self.model).where(self.model.id == id).values(**data)
         result = await self.session.execute(stmt)
         await self.session.commit()
         return result.scalar_one_or_none()
--- db/dao/users.py
+++ db/dao/users.py
@@ -13,7 +13,7 @@ class UserDAO(BaseDAO[User]):
         result = await self.session.execute(smt)
         return result.scalar_one_or_none()
 
-    async def update_user_by_user_id(self, user_id: int, data: dict[str, Any]) -> model:
+    async def update_user_by_user_id(self, user_id: int, **data) -> model:
         stmt = update(self.model).where(self.model.user_id == user_id).values(**data)
         result = await self.session.execute(stmt)
         await self.session.commit()
